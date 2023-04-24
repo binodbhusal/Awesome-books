94ca647c393f8f2389dc15055ab0453a6836d09c
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-04-24 16:50:54 +0100

    amend eslint errors

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 let bookList = [];
 
-const bookItem = document.getElementById("book-list");
-const addBtn = document.getElementById("addBtn");
-const bookTitle = document.getElementById("title");
-const bookAuthor = document.getElementById("author");
-const error = document.getElementById("error");
-const form = document.getElementById("form");
+const bookItem = document.getElementById('book-list');
+const addBtn = document.getElementById('addBtn');
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const error = document.getElementById('error');
+const form = document.getElementById('form');
 
 const updateLocalStorage = (data) => {
-  localStorage.setItem("storage-book", JSON.stringify(data));
+  localStorage.setItem('storage-book', JSON.stringify(data));
 };
 
-const getLocalStorage = () => JSON.parse(localStorage.getItem("storage-book"));
+const getLocalStorage = () => JSON.parse(localStorage.getItem('storage-book'));
 
 const displayBooks = () => {
-  bookItem.innerHTML = "";
+  bookItem.innerHTML = '';
   bookList.forEach((book) => {
-    const bookListWrap = document.createElement("ul");
+    const bookListWrap = document.createElement('ul');
     bookListWrap.innerHTML = `
             <li>
             <p>Author: ${book.author}</p>
@@ -36,13 +36,13 @@ const initialize = () => {
 // we add new books here
 const addNewBook = (title, author) => {
   const book = { title, author };
-  //check if there is duplicate
+  // check if there is duplicate
   const checkBooks = bookList.find((book) => book.title === title);
   const checkAuthors = bookList.find((book) => book.author === author);
   if (checkBooks && checkAuthors) {
-    error.innerText = "There is already a duplicate here!!";
+    error.innerText = 'There is already a duplicate here!!';
   } else {
-    error.innerHTML = "";
+    error.innerHTML = '';
   }
   bookList.push(book);
   updateLocalStorage(bookList);
@@ -57,17 +57,17 @@ const deleteBook = (title) => {
   displayBooks();
 };
 
-document.addEventListener("click", (e) => {
-  const deleteBtn = e.target.closest(".btnRemove");
+document.addEventListener('click', (e) => {
+  const deleteBtn = e.target.closest('.btnRemove');
   if (deleteBtn) {
     deleteBook(deleteBtn.id);
   }
 });
 
-addBtn.addEventListener("click", (e) => {
+addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   if (bookTitle.value.length === 0 || bookAuthor.value.length === 0) {
-    error.innerText = `The values you provided are empty`;
+    error.innerText = 'The values you provided are empty';
   } else {
     addNewBook(bookTitle.value, bookAuthor.value);
     form.reset();
