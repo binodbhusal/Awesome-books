b8988dadd623737d4a19861254ab3918481f3556
Vangelis <ev.fotiades@yahoo.com>
2023-04-24 18:30:13 +0300

    include function for deleting book from storage and display

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ const updateLocalStorage = (data) => {
 const getLocalStorage = () => JSON.parse(localStorage.getItem("storage-book"));
 
 const displayBooks = () => {
+  bookItem.innerHTML = "";
   bookList.forEach((book) => {
-    bookItem.insertAdjacentHTML(
-      "afterend",
-      `
+    const bookListWrap = document.createElement("ul");
+    bookListWrap.innerHTML = `
             <li>
             <p>Author: ${book.author}</p>
             <p>Title: ${book.title}</p>
-            <button id="${book.key}"class="btnRemove" type="button">Remove</button>
+            <button id="${book.title}" class="btnRemove" type="button">Remove</button>
           </li>
-          <hr>`
-    );
+          <hr>`;
+    bookItem.appendChild(bookListWrap);
   });
 };
 
@@ -36,6 +36,7 @@ const initialize = () => {
 // we add new books here
 const addNewBook = (title, author) => {
   const book = { title, author };
+  //check if there is duplicate
   const checkBooks = bookList.find((book) => book.title === title);
   const checkAuthors = bookList.find((book) => book.author === author);
   if (checkBooks && checkAuthors) {
@@ -48,19 +49,27 @@ const addNewBook = (title, author) => {
   displayBooks();
 };
 
-error.innerHTML = "";
 initialize();
 
+const deleteBook = (title) => {
+  bookList = bookList.filter((it) => it.title !== title);
+  updateLocalStorage(bookList);
+  displayBooks();
+};
+
+document.addEventListener("click", (e) => {
+  const deleteBtn = e.target.closest(".btnRemove");
+  if (deleteBtn) {
+    deleteBook(deleteBtn.id);
+  }
+});
+
 addBtn.addEventListener("click", (e) => {
-  console.log("it is clicked!");
   e.preventDefault();
   if (bookTitle.value.length === 0 || bookAuthor.value.length === 0) {
     error.innerText = `The values you provided are empty`;
   } else {
     addNewBook(bookTitle.value, bookAuthor.value);
-    console.log(
-      "the values are added to local storage and are displayed above"
-    );
     form.reset();
   }
 });
