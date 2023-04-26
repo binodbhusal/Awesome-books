ded153464a5abda5fcbc6e01b9bd4adba4b340c2
Vangelis <ev.fotiades@yahoo.com>
2023-04-26 11:42:51 +0300

    add listeners and define variables

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const bookAuthor = document.getElementById('author');
 const error = document.getElementById('error');
 const form = document.getElementById('form');
 
+const listBtn = document.getElementById('list-link');
+const formBtn = document.getElementById('form-link');
+const contactBtn = document.getElementById('contact-link');
+
+const listSection = document.getElementById('list');
+const newBookSection = document.getElementById('form');
+const contactSection = document.getElementById('contact');
+
 class Book {
   constructor() {
     this.bookList = JSON.parse(localStorage.getItem('storage-book')) || [];
@@ -52,7 +60,7 @@ const displayBooks = () => {
         <tr>
           <td>${book.title} by ${book.author}</td>
           <td><button class="btnRemove" id=${book.id}>Remove</button></td>
-        </tr>`,
+        </tr>`
     );
   });
 };
@@ -67,7 +75,7 @@ const emptyCell = () => {
 const checkDoubles = () => {
   const checkBooks = bookList.find((book) => book.title === bookTitle.value);
   const checkAuthors = bookList.find(
-    (book) => book.author === bookAuthor.value,
+    (book) => book.author === bookAuthor.value
   );
   if (checkBooks && checkAuthors) {
     error.innerText = 'There is already a duplicate here!!';
@@ -93,6 +101,36 @@ document.addEventListener('click', (e) => {
     bookList = books.getBook();
     displayBooks();
   }
+});
+
+const hideList = () => {
+  listSection.classList.add('hidden');
+};
+
+const hideForm = () => {
+  newBookSection.classList.add('hidden');
+};
+
+const hideContact = () => {
+  contactSection.classList.add('hidden');
+};
+
+listBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  hideForm();
+  hideContact();
+});
+
+formBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  hideList();
+  hideContact();
+});
+
+contactBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  hideList();
+  hideForm();
 });
 
 displayBooks();
